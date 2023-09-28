fcbc675fd94402ab7e8e8dbafd41485a8a883bb7
YosepH2OW <132586515+YosepH2OW@users.noreply.github.com>
2023-09-28 19:47:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
-// Creazione di un server HTTP
-const server = http.createServer((req, res) => {
-  // Configura l'intestazione di risposta con il tipo di contenuto
-  res.setHeader('Content-Type', 'text/plain');
-  
-  // Verifica il percorso richiesto
-  if (req.url === '/') {
-    // Se la richiesta è per la radice '/', invia 'Hello World!'
-    res.end('Hello World!\n');
-  } else if (req.url === '/about') {
-    // Se la richiesta è per '/about', invia un messaggio sull'about
-    res.end('About Page\n');
-  } else {
-    // Se il percorso richiesto non corrisponde a nessuno dei precedenti, invia un messaggio di errore 404
-    res.statusCode = 404;
-    res.end('404 Not Found\n');
-  }
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-// Definisci la porta su cui il server ascolterà
-const port = 3000;
-
-// Avvia il server sulla porta specificata
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
